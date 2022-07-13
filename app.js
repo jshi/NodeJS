13766ef3f5be9fa841e5b4fe5ccd18b9f41b1d7f
Jim Shi <Jim.Shi@firsttechfed.com>
2022-07-13 09:52:20 -0700

    initial code with high level site layout and sighup

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var path = require("path");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser")
+var passport = require("passport");
+var session = require("express-session");
+var flash = require("connect-flash");
+
+var params = require("./params/params");
+var setUpPassport = require("./setuppassport");
+
+var app = express();
+
+mongoose.connect(params.DATABASECONNECTION);
+
+setUpPassport();
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(session({
+   secret:'dfejiry239r8edfh$vk*&sdvnkmkd',
+   resave:false,
+   saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use("/", require("./routes/web"));
+app.use("/api", require("./routes/api"));
+
+app.listen(app.get("port"),function() {
+   console.log("Server started on port " + app.get("port"));
+});
---
+++ routes/web/home.js
@@ -0,0 +1,77 @@
+/**
+ * Stores all routes for "/"
+ */
+
+var express = require('express');
+var passport = require('passport');
+
+var User = require('../../models/user');
+
+var router = express.Router();
+
+// routes
+router.get('/', function(req, res){
+    res.render('home/');
+});
+
+router.get('/home', function(req, res){
+    res.render('home/home');
+});
+
+router.get('/about', function(req, res){
+    res.render('home/about');
+});
+
+router.get('/login', function(req, res){
+    res.render('home/login');
+});
+
+router.post('/login', passport.authenticate('login', {
+    successRedirect:'/',
+    failureRedirect:'/login',
+    failureFlash:true
+}));
+
+router.get('/logout', function(req, res){
+    //logout
+    req.logout(function(err){
+        if (err) { return next(err); }
+        //redirect
+        res.redirect('/home');
+    });
+});
+
+router.get('/signup', function(req, res){
+    res.render('home/signup');
+});
+
+router.post('/signup', function(req, res, next){
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    //check if user already exists
+    User.findOne({email: email}, function(err, user){
+        if (err) { return next(err); }
+        //user already exists (with specified email)
+        if (user) {
+            req.flash('error', 'There is already an account with this email');
+            return res.redirect('/signup');
+        }
+
+        //create new user object
+        var newUser = new User({
+            username:username,
+            email:email,
+            password:password
+        });
+
+        newUser.save(next);
+    });
+}, passport.authenticate('login', {
+    successRedirect:'/',
+    failureRedirect:'/signup',
+    failureFlash:true
+}));
+
+module.exports = router;
