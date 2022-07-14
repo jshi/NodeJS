c43dec655e185fcc4e93ccfe345b0509a8c92f96
Jim Shi <Jim.Shi@firsttechfed.com>
2022-07-13 22:44:07 -0700

    updated with posts read and edit pages

--- routes/web/post.js
+++ routes/web/post.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 
 var ensureAuthenticated = require("../../auth/auth").ensureAuthenticated;
 
@@ -8,29 +8,66 @@ var router = express.Router();
 
 router.use(ensureAuthenticated);
 
-router.get('/', function(req, res) {
-    Post.find({userID:req.user._id}).exec(function (err, posts) {
-        if (err) { console.log(err) };
+router.get("/", function(req, res){
+    Post.find({userID:req.user._id}).exec(function(err, posts){
+        if(err){console.log(err);}
 
         res.render("post/posts", {posts:posts});
     });
 });
 
-router.get('/add', function(req, res) {
-    res.render('post/addpost');
+
+router.get("/add", function(req, res){
+    res.render("post/addpost");
 });
 
-router.post("/add", function (req, res) {
+router.post("/add", function(req, res){
+
     var newPost = new Post({
-        title: req.body.title,
-        content: req.body.content,
-        userID: req.user._id
+        title:req.body.title,
+        content:req.body.content,
+        userID:req.user._id
     });
 
-    newPost.save(function (err, post) {
-        if (err) { console.log(err)};
+    newPost.save(function(err,post){
+        if(err){console.log(err);}
         res.redirect("/post");
     });
+
+});
+
+//: means a route parameter it could be anything and it's often an ID
+// localhost:3000/posts/12345 --> fetch the post with id 12345
+router.get("/:postId", function(req,res){
+    Post.findById(req.params.postId).exec(function(err, post){
+        res.render("post/detailpost",{post:post});
+    });
+});
+
+router.get("/edit/:postId", function(req,res){
+    Post.findById(req.params.postId).exec(function(err, post){
+        res.render("post/editpost",{post:post});
+    });
+});
+
+router.post("/update", async function(req, res){
+    const post = await Post.findById(req.body.postid);
+
+    post.title = req.body.title;
+    post.content = req.body.content;
+
+    // post.save()
+
+    try {
+        let savePost = await post.save();
+        console.log("savepost", savePost);
+        res.redirect("/post/" + req.body.postid);
+
+    } catch (err) {
+        console.log("error happened");
+        res.status(500).send(err);
+    }
+
 });
 
 module.exports = router;
